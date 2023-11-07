02f1a677396974720c769f66843534483188816d
anjali <singhanjalirkl16@gmail.com>
2023-11-07 07:49:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import "./styles.css";
+import Routing from "./Routing";
+function App() {
+  return (
+    <div className="App">
+    <Routing />
+    </div>
+  );
+}
+
+export default App;
